66fc49c4a195503d6beb8020ef0289a9c2b2902c
Renze Nicolai <renze@rnplus.nl>
2017-11-01 13:13:08 +0100

    Another ESP32 fix

--- Homey.cpp
+++ Homey.cpp
@@ -880,7 +880,11 @@ void rcSecureAnalogWrite(const String& pin, int state)
 	uint8_t p = rcMapPin(pin);
 	if (p==255) return;
 	
+#ifndef ARDUINO_ARCH_ESP32
 	analogWrite(p, state);
+#else
+	DEBUG_PRINTLN("analog write is not supported on this platform");
+#endif
 }
 
 int rcSecureAnalogRead(const String& pin)
