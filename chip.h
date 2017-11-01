1e180242c172c36af4dc92b63481168e1908bcab
Renze Nicolai <renze@rnplus.nl>
2017-11-01 13:10:57 +0100

    Fix for ESP32

--- chip.h
+++ chip.h
@@ -42,101 +42,120 @@
 #endif
 
 static const uint8_t analog_input_map[] = {
-#if (NUM_ANALOG_INPUTS>0)
-	A0
-#endif
-#if (NUM_ANALOG_INPUTS>1)
-	,A1
-#endif
-#if (NUM_ANALOG_INPUTS>2)
-	,A2
-#endif
-#if (NUM_ANALOG_INPUTS>3)
-	,A3
-#endif
-#if (NUM_ANALOG_INPUTS>4)
-	,A4
-#endif
-#if (NUM_ANALOG_INPUTS>5)
-	,A5
-#endif
-#if (NUM_ANALOG_INPUTS>6)
-	,A6
-#endif
-#if (NUM_ANALOG_INPUTS>7)
-	,A7
-#endif
-#if (NUM_ANALOG_INPUTS>8)
-	,A8
-#endif
-#if (NUM_ANALOG_INPUTS>9)
-	,A9
-#endif
-#if (NUM_ANALOG_INPUTS>10)
-	,A10
-#endif
-#if (NUM_ANALOG_INPUTS>11)
-	,A11
-#endif
-#if (NUM_ANALOG_INPUTS>12)
-	,A12
-#endif
-#if (NUM_ANALOG_INPUTS>13)
-	,A13
-#endif
-#if (NUM_ANALOG_INPUTS>14)
-	,A14
-#endif
-#if (NUM_ANALOG_INPUTS>15)
-	,A15
-#endif
-#if (NUM_ANALOG_INPUTS>16)
-	,A16
-#endif
-#if (NUM_ANALOG_INPUTS>17)
-	,A17
-#endif
-#if (NUM_ANALOG_INPUTS>18)
-	,A18
-#endif
-#if (NUM_ANALOG_INPUTS>19)
-	,A19
-#endif
-#if (NUM_ANALOG_INPUTS>20)
-	,A20
-#endif
-#if (NUM_ANALOG_INPUTS>21)
-	,A21
-#endif
-#if (NUM_ANALOG_INPUTS>22)
-	,A22
-#endif
-#if (NUM_ANALOG_INPUTS>23)
-	,A23
-#endif
-#if (NUM_ANALOG_INPUTS>24)
-	,A24
-#endif
-#if (NUM_ANALOG_INPUTS>25)
-	,A25
-#endif
-#if (NUM_ANALOG_INPUTS>26)
-	,A26
-#endif
-#if (NUM_ANALOG_INPUTS>27)
-	,A27
-#endif
-#if (NUM_ANALOG_INPUTS>28)
-	,A28
-#endif
-#if (NUM_ANALOG_INPUTS>29)
-	,A29
-#endif
-#if (NUM_ANALOG_INPUTS>30)
-	,A30
-#endif
-#if (NUM_ANALOG_INPUTS>31)
-	,A31
+#if defined(ARDUINO_ARCH_ESP32)
+	A0,
+	A3,
+	A4,
+	A5,
+	A6,
+	A7,
+	A10,
+	A11,
+	A12,
+	A13,
+	A14,
+	A15,
+	A16,
+	A17,
+	A18,
+	A19	
+#else
+	#if (NUM_ANALOG_INPUTS>0)
+		A0
+	#endif
+	#if (NUM_ANALOG_INPUTS>1)
+		,A1
+	#endif
+	#if (NUM_ANALOG_INPUTS>2)
+		,A2
+	#endif
+	#if (NUM_ANALOG_INPUTS>3)
+		,A3
+	#endif
+	#if (NUM_ANALOG_INPUTS>4)
+		,A4
+	#endif
+	#if (NUM_ANALOG_INPUTS>5)
+		,A5
+	#endif
+	#if (NUM_ANALOG_INPUTS>6)
+		,A6
+	#endif
+	#if (NUM_ANALOG_INPUTS>7)
+		,A7
+	#endif
+	#if (NUM_ANALOG_INPUTS>8)
+		,A8
+	#endif
+	#if (NUM_ANALOG_INPUTS>9)
+		,A9
+	#endif
+	#if (NUM_ANALOG_INPUTS>10)
+		,A10
+	#endif
+	#if (NUM_ANALOG_INPUTS>11)
+		,A11
+	#endif
+	#if (NUM_ANALOG_INPUTS>12)
+		,A12
+	#endif
+	#if (NUM_ANALOG_INPUTS>13)
+		,A13
+	#endif
+	#if (NUM_ANALOG_INPUTS>14)
+		,A14
+	#endif
+	#if (NUM_ANALOG_INPUTS>15)
+		,A15
+	#endif
+	#if (NUM_ANALOG_INPUTS>16)
+		,A16
+	#endif
+	#if (NUM_ANALOG_INPUTS>17)
+		,A17
+	#endif
+	#if (NUM_ANALOG_INPUTS>18)
+		,A18
+	#endif
+	#if (NUM_ANALOG_INPUTS>19)
+		,A19
+	#endif
+	#if (NUM_ANALOG_INPUTS>20)
+		,A20
+	#endif
+	#if (NUM_ANALOG_INPUTS>21)
+		,A21
+	#endif
+	#if (NUM_ANALOG_INPUTS>22)
+		,A22
+	#endif
+	#if (NUM_ANALOG_INPUTS>23)
+		,A23
+	#endif
+	#if (NUM_ANALOG_INPUTS>24)
+		,A24
+	#endif
+	#if (NUM_ANALOG_INPUTS>25)
+		,A25
+	#endif
+	#if (NUM_ANALOG_INPUTS>26)
+		,A26
+	#endif
+	#if (NUM_ANALOG_INPUTS>27)
+		,A27
+	#endif
+	#if (NUM_ANALOG_INPUTS>28)
+		,A28
+	#endif
+	#if (NUM_ANALOG_INPUTS>29)
+		,A29
+	#endif
+	#if (NUM_ANALOG_INPUTS>30)
+		,A30
+	#endif
+	#if (NUM_ANALOG_INPUTS>31)
+		,A31
+	#endif
 #endif
 };
 
