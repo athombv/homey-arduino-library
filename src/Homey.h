937efc280dc6b646ca9f983b062deb2a025a6500
Renze Nicolai <renze@rnplus.nl>
2018-01-05 13:11:56 +0100

    Fix bug that caused setCapabiltyValue not to work when the value type is boolean. Also fixes compiler warning about the version constant in the descriptor.

--- src/Homey.h
+++ src/Homey.h
@@ -17,7 +17,7 @@
 /* -------------- DO NOT EDIT ANYTHING BELOW THIS LINE!  -------------- */
 /* (If you do you might break compatibility with the Homeyduino app...) */
 
-#define HOMEYDUINO_VERSION "1.0.1"
+#define HOMEYDUINO_VERSION "1.0.2"
 
 #define ENDPOINT_MAX_SIZE 17 //16 + null
 #define ARGUMENT_MAX_SIZE 65 //64 + null
